824d8ae2693533ea6c178fd4118beb37cb1fe7ce
Tejas Mukherjee <mukherjeetejas21@gmail.com>
2021-07-13 22:49:50 +0530

    contact page

--- src/components/useScroll.js
+++ src/components/useScroll.js
@@ -3,7 +3,7 @@ import { useAnimation } from "framer-motion";
 
 export const useScroll = () => {
   const controls = useAnimation();
-  const [element, view] = useInView({ threshold: 0.6 });
+  const [element, view] = useInView({ threshold: 0.2 });
   if (view) {
     controls.start("show");
   } else {
--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -1,19 +1,75 @@
 import React from "react";
+import styled from "styled-components";
+import { pageAnimation, titleAnim } from "../Animation";
 //Animations
 import { motion } from "framer-motion";
-import { pageAnimation } from "../Animation";
 
 const ContactUs = () => {
   return (
-    <motion.div
+    <ContactStyle
       exit="exit"
       variants={pageAnimation}
       initial="hidden"
       animate="show"
+      style={{ background: "#fff" }}
     >
-      <h1>Hello call me maybe</h1>
-    </motion.div>
+      <Title>
+        <Hide>
+          <motion.h2 variants={titleAnim}>Get in touch.</motion.h2>
+        </Hide>
+      </Title>
+      <div>
+        <Hide>
+          <Social variants={titleAnim}>
+            <Circle />
+            <a href = "https://www.linkedin.com/in/tejas-mukherjee/"><h2>Linkedin</h2></a>
+          </Social>
+        </Hide>
+        <Hide>
+          <Social variants={titleAnim}>
+            <Circle />
+            <a href="https://github.com/mukherjeetejas/"><h2>Github</h2></a>
+          </Social>
+        </Hide>
+        <Hide>
+          <Social variants={titleAnim}>
+            <Circle />
+            <a href="https://www.instagram.com/tejasmukherjee/"><h2>Instagram</h2></a>
+          </Social>
+        </Hide>
+      </div>
+    </ContactStyle>
   );
 };
+
+const ContactStyle = styled(motion.div)`
+  padding: 5rem 10rem;
+  color: #353535;
+  min-height: 90vh;
+`;
+const Title = styled.div`
+  margin-bottom: 4rem;
+  color: black;
+`;
+const Hide = styled.div`
+  overflow: hidden;
+`;
+const Circle = styled.div`
+  border-radius: 50%;
+  width: 3rem;
+  height: 3rem;
+  background: #353535;
+`;
+const Social = styled(motion.div)`
+  display: flex;
+  align-items: center;
+  a{
+    text-decoration: none;
+    color: #353535;
+  }
+  h2 {
+    margin: 1rem;
+  }
+`;
 
 export default ContactUs;
