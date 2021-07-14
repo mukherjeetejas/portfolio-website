b1ecc5e76fb04b545a0b4f5675044f16d812b14a
Tejas Mukherjee <mukherjeetejas21@gmail.com>
2021-07-14 17:12:07 +0530

    nav line

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,8 +2,13 @@ import React from "react";
 import styled from "styled-components";
 //Links
 import { Link } from "react-router-dom";
+import {motion} from "framer-motion"
+//Nav line
+import { useLocation } from "react-router-dom";
+
 
 const Nav = () => {
+  const {pathname} = useLocation()
   return (
     <StyledNav>
       <h1>
@@ -14,12 +19,27 @@ const Nav = () => {
       <ul>
         <li>
           <Link to="/">About Me</Link>
+          <Line
+            transition={{ duration: 0.75 }}
+            initial={{ width: "0%" }}
+            animate={{ width: pathname === "/" ? "50%" : "0%" }}
+          />
         </li>
         <li>
           <Link to="/work">My Work</Link>
+          <Line
+            transition={{ duration: 0.75 }}
+            initial={{ width: "0%" }}
+            animate={{ width: pathname.startsWith("/work") ? "50%" : "0%" }}
+          />
         </li>
         <li>
           <Link to="/contact">Contact Me</Link>
+          <Line
+            transition={{ duration: 0.75 }}
+            initial={{ width: "0%" }}
+            animate={{ width: pathname === "/contact" ? "50%" : "0%" }}
+          />
         </li>
       </ul>
     </StyledNav>
@@ -69,6 +89,18 @@ const StyledNav = styled.nav`
         padding: 0;
       }
     }
+  }
+`;
+
+const Line = styled(motion.div)`
+  height: 0.3rem;
+  background: #23d997;
+  width: 0%;
+  position: absolute;
+  bottom: -80%;
+  left: 60%;
+  @media (max-width: 1300px) {
+    left: 0%;
   }
 `;
 
